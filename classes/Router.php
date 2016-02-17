71a6bed7e8a12577ed97840e388f75e7522dde52
Yi Zhao <zhao1max@gmail.com>
2016-02-17 13:19:01 -0500

    add page class

---
+++ classes/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class Router
+ * @Author: Yi Zhao
+ */
+class Router {
+
+}
+
+var_dump($_SERVER['REQUEST_URI']);
---
+++ classes/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Yi Zhao
+ * Date: 16/02/16
+ */
+?>
+<h1>header</h1>
