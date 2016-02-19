d0013fe556ddffb76cc66fa21e8cd217ada8ef54
Yi Zhao <zhao1max@gmail.com>
2016-02-19 18:18:24 -0500

    add dbconfig

--- Classes/DB.php
+++ Classes/DB.php
@@ -6,17 +6,12 @@ namespace Project\Classes;
  *
  */
 
-define('SERVER', 'localhost:3306');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'php_project');
-
 class DB {
   private static $_db;
   public static function getDB() {
     if (!self::$_db) {
       try {
-        self::$_db = new \PDO('mysql:host=' . SERVER . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+        self::$_db = new \PDO('mysql:host=' . DbConfig::SERVER . ';dbname=' . DbConfig::DB_NAME, DbConfig::DB_USER, DbConfig::DB_PASS);
       } catch (\PDOException $e) {
         echo 'database error: ', $e->getMessage();
       }
