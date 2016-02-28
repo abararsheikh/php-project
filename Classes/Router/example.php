bf13c820300597d01909fae3409ab9fb9d63e9e7
Yi Zhao <zhao1max@gmail.com>
2016-02-28 00:47:02 -0500

    update expample

--- Classes/Router/example.php
+++ Classes/Router/example.php
@@ -1,21 +1,27 @@
 <?php
 use Project\Classes\Router\Nav;
 
-include '../../autoloader.php';
+include "Nav.php";
+include '../Helper.php';
+include 'Route.php';
+
+// Set root here
+define('ROOT', '');
 
 // A function makes example cleaner.
 function display($word) {
   return function () use ($word) { echo $word; };
 }
 
-Nav::group('/Classes/Router as Router Example', function () {
+Nav::group(ROOT . '/Classes/Router as Router Example', function () {
   Nav::get('/ as Home', display('Home'));
   Nav::get('/login as Login', display('Login'));
   Nav::get('/register as Register', display('Register'));
-  Nav::group('/Classes/Router/register as Register', function () {
 
+  Nav::group(ROOT . '/Classes/Router/register as Register', function () {
     Nav::get('/1 as sub menu 1', display('register sub page 1'));
-    Nav::group('/Classes/Router/register/1 as sub menu 1', function () {
+
+    Nav::group(ROOT . '/Classes/Router/register/1 as sub menu 1', function () {
       Nav::get('/1 as R11', display('R11'));
     });
     Nav::get('/2 as sub menu 2', display('register sub page 2'));
