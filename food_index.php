a71ce1ee62bef5a1bf60ff135abb70106722c1f3
LenaYu2016 <475099436yu@gmail.com>
2016-03-04 04:28:23 +0800

    dood update

---
+++ food_index.php
@@ -0,0 +1,44 @@
+<?php
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link href="bootstrap.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="jquery.min.js" ></script>
+    <script src="bootstrap.min.js" ></script>
+    <link rel="stylesheet" href="food-index.css" />
+</head>
+<main>
+    <div class="container-fluid">
+        <div class="page-header">
+            <h1>Food Menu</h1>
+        </div>
+        <div class="row ctlist">
+        <div class="col-sm-4 col-md-4">.col-sm-4</div>
+            </div>
+        <div class="menu">
+            <div class="row">
+
+            <div class="col-sm-2 col-md-2  image"><img src="food1.jpg" class="img-responsive img-rounded" width="225" height="180"/>
+
+            </div>
+            <div class="col-sm-2 col-md-2  image"><img src="food2.jpg" class="img-responsive img-rounded" width="225" height="180"/>
+
+            </div>
+            <div class="col-sm-2 col-md-2  image"><img src="food3.jpg"  class="img-responsive img-rounded" width="225" height="180"/>
+
+            </div>
+            <div class="col-sm-2 col-md-2  image"><img src="food4.jpg"  class="img-responsive img-rounded" width="225" height="180"/>
+
+            </div>
+
+        </div>
+        <div class="row">
+        <div class="col-sm-2 col-md-2  image"><img src="food4.jpg"  class="img-responsive img-rounded" width="225" height="180"/>
+</div>
+        </div>
+    </div>
+
+    </div>
+</main>
