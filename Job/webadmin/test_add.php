698f373e0d8567144c50058aba838168090f4344
s08150br <s08150br@gmail.com>
2016-03-09 01:13:59 -0500

    change
    change

--- Job/webadmin/test_add.php
+++ Job/webadmin/test_add.php
@@ -11,7 +11,7 @@ if($act=='add')
   {
 
     echo  "<script language='javascript'>";
-    echo  "alert('提交成功');";
+    echo  "alert('sumbit ok');";
     echo  "location.href='Admin_Index_main.php'";
     echo "</script>";
   }
@@ -20,39 +20,36 @@ if($act=='add')
 ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
-<title>招聘信息添加</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Hire information</title>
 <link href="Css_Main.css" rel="stylesheet" type="text/css">
 <style type="text/css">
-<!--
-.style2 {color: #FF0000}
-.style1 {color: #CCCCCC}
--->
+
 </style>
 </head>
-<script language="javascript" type="text/javascript" src="My97DatePicker/WdatePicker.js"></script>
+
 <script language="JavaScript">
 function  Check_stu()
 {
 
 	if (document.form1.zhiwei.value == "")
-	 {   alert("请输入职位");
+	 {   alert("Enter job");
 		document.form1.zhiwei.focus();
 		return false;
 	 }
 	if (document.form1.qixian.value == "")
-	 {   alert("请输入时间");
+	 {   alert("Enter time");
 		document.form1.qixian.focus();
 		return false;
 	 }
 	 if (document.form1.renshu.value == "")
-	 {   alert("请输入招聘人数");
+	 {   alert("Enter nunmber");
 		document.form1.renshu.focus();
 		return false;
 	 }
 	
 	 if (document.form1.yaoqiu.value == "")
-	 {   alert("请填写招聘要求");
+	 {   alert("Enter Hire Requirement");
 		document.form1.yaoqiu.focus();
 		return false;
 	 }	 
@@ -67,38 +64,38 @@ function  Check_stu()
 <form name="form1" action="test_add.php?act=add" method="post" enctype="multipart/form-data">
 <table width="98%"  border="0" align="center" cellpadding="0" cellspacing="1" class="border">
   <tr class="title_top">
-    <td colspan="2"><div align="center">添加招聘信息信息</div></td>
+    <td colspan="2"><div align="center">Add Hire Requirement</div></td>
   </tr>
   <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>问题题目：</strong></div></td>
+    <td width="20%"><div align="right"><strong>Question Title：</strong></div></td>
     <td><input name="wenti" type="text"  size="40"></td>
   </tr>
   <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>问题A选项：</strong></div></td>
+    <td width="20%"><div align="right"><strong>A：</strong></div></td>
     <td><input name="answer1" type="text"  size="40"></td>
   </tr>
     <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>问题B选项：</strong></div></td>
+    <td width="20%"><div align="right"><strong>B：</strong></div></td>
     <td><input name="answer2" type="text"  size="40"></td>
   </tr>
   <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>问题C选项：</strong></div></td>
+    <td width="40%"><div align="right"><strong>C：</strong></div></td>
     <td><input name="answer3" type="text"  size="40"></td>
   </tr>
     <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>问题D选项：</strong></div></td>
+    <td width="40%"><div align="right"><strong>D：</strong></div></td>
     <td><input name="answer4" type="text"  size="40"></td>
   </tr>
       <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>设置问题答案例:(a、b、c、d)：</strong></div></td>
+    <td width="40%"><div align="right"><strong>answer:(a、b、c、d)：</strong></div></td>
     <td><input name="rights" type="text"  size="40"></td>
   </tr>
        <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>题目分数</strong></div></td>
+    <td width="40%"><div align="right"><strong>Mark</strong></div></td>
     <td><input name="fenshu" type="text"  size="40"></td>
   </tr>
         <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>名字</strong></div></td>
+    <td width="40%"><div align="right"><strong>Name</strong></div></td>
     <td><input name="name" type="text"  size="40"></td>
   </tr>
 </table>
@@ -106,9 +103,9 @@ function  Check_stu()
   <tr>
     <td height="40">
       <div align="center"><span class="style1">
-        <input name="Submit" type="submit" id="Submit" value="添加" onClick="return Check_stu()">
+        <input name="Submit" type="submit" id="Submit" value="ADD" onClick="return Check_stu()">
 &nbsp;
-        <input name="reset" type="reset" id="reset" value="取消">
+        <input name="reset" type="reset" id="reset" value="Cancel">
     </span> </div></td>
   </tr>
 </table></form>
--- Job/webadmin/web_add.php
+++ Job/webadmin/web_add.php
@@ -9,7 +9,7 @@ if($act=='add')
   if(mysql_query($sql))
   {
     echo  "<script language='javascript'>";
-    echo  "alert('提交成功');";
+    echo  "alert('Sumbit success');";
     echo  "location.href='Admin_Index_main.php'";
     echo "</script>";
   }
@@ -19,7 +19,7 @@ if($act=='add')
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=gb2312">
-<title>招聘信息添加</title>
+<title>Add Hire information</title>
 <link href="Css_Main.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 <!--
@@ -34,23 +34,23 @@ function  Check_stu()
 {
 
 	if (document.form1.zhiwei.value == "")
-	 {   alert("请输入职位");
+	 {   alert("Enter JOb title");
 		document.form1.zhiwei.focus();
 		return false;
 	 }
 	if (document.form1.qixian.value == "")
-	 {   alert("请输入时间");
+	 {   alert("Enter time");
 		document.form1.qixian.focus();
 		return false;
 	 }
 	 if (document.form1.renshu.value == "")
-	 {   alert("请输入招聘人数");
+	 {   alert("Enter numbet you want");
 		document.form1.renshu.focus();
 		return false;
 	 }
 	
 	 if (document.form1.yaoqiu.value == "")
-	 {   alert("请填写招聘要求");
+	 {   alert("Enter your reqiorement");
 		document.form1.yaoqiu.focus();
 		return false;
 	 }	 
@@ -65,22 +65,22 @@ function  Check_stu()
 <form name="form1" action="web_add.php?act=add" method="post" enctype="multipart/form-data">
 <table width="98%"  border="0" align="center" cellpadding="0" cellspacing="1" class="border">
   <tr class="title_top">
-    <td colspan="2"><div align="center">添加招聘信息信息</div></td>
+    <td colspan="2"><div align="center">Add Hire information</div></td>
   </tr>
   <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>招聘职位：</strong></div></td>
+    <td width="20%"><div align="right"><strong>Job title：</strong></div></td>
     <td><input name="zhiwei" type="text"  size="40"></td>
   </tr>
   <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>招聘期限：</strong></div></td>
+    <td width="20%"><div align="right"><strong>Limit time：</strong></div></td>
     <td><input name="qixian" type="text"  size="40"></td>
   </tr>
     <tr class="tdbg">
-    <td width="20%"><div align="right"><strong>招聘人数：</strong></div></td>
+    <td width="20%"><div align="right"><strong>Hire number：</strong></div></td>
     <td><input name="renshu" type="text"  size="40"></td>
   </tr>
   <tr class="tdbg">
-    <td width="40%"><div align="right"><strong>招聘要求：</strong></div></td>
+    <td width="40%"><div align="right"><strong>Hire requirement：</strong></div></td>
     <td><textarea name="yaoqiu" rows="5" cols="36"></textarea></td>
   </tr>
     
@@ -89,9 +89,9 @@ function  Check_stu()
   <tr>
     <td height="40">
       <div align="center"><span class="style1">
-        <input name="Submit" type="submit" id="Submit" value="添加" onClick="return Check_stu()">
+        <input name="Submit" type="submit" id="Submit" value="Add" onClick="return Check_stu()">
 &nbsp;
-        <input name="reset" type="reset" id="reset" value="取消">
+        <input name="reset" type="reset" id="reset" value="cancel">
     </span> </div></td>
   </tr>
 </table></form>
