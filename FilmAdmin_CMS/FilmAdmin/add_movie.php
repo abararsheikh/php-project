4a8ade404fb5e00133a0e631b03040b8f6c5b3bd
abrar <er.abrar@gmail.com>
2016-03-22 10:05:09 -0400

    adminCMS edited

--- FilmAdmin_CMS/FilmAdmin/add_movie.php
+++ FilmAdmin_CMS/FilmAdmin/add_movie.php
@@ -1,37 +1,42 @@
 <?php
-
+//connect to database
 use Project\Classes\DB\DB;
+include '../../autoloader.php';
 $db = DB::getDB();
-// Get the value from the form
-//require_once('database.php');
 
+//require_once('database.php');
+// Get the value from the form
 $film_id = $_POST['film_id'];
 $title = $_POST['title'];
 $releaseDate = $_POST['releaseDate'];
 $director = $_POST['director'];
 $cast = $_POST['cast'];
-//$img = $_POST['img1'];
+//$img = $_POST['img'];
 
-// ====Get the Image
+// Image
 $file_get = $_FILES['img1']['name'];
 $temp = $_FILES['img1']['tmp_name'];
 
-$file_to_saved = "../Assets/image/HomePage/".$file_get;
+$file_to_saved = "/Assets/image/HomePage/".$file_get;
 
 //Documents folder, should exist in your host in there you're going to save the file just uploaded
 
 move_uploaded_file($temp, $file_to_saved);
 
-//==================
+//echo $file_to_saved;
+
+
+
+ // Image part ends here
 
 // Validate inputs
 if (empty($title) || empty($releaseDate) || empty($cast) || empty($director) || empty($file_get) ) {
-    $error = "Invalid Movie data. Check all fields and try again.";
+    $error = "Invalid Movie data. Check and fill all fields and then try again.";
     echo $error;
 } else {
-    // If valid, update the movie to the database
+    // If valid, Add the movie to the database
 
-    $query = "UPDATE moviefeature SET title ='$title',releaseDate='$releaseDate',director ='$director',cast='$cast',img='$file_to_saved' WHERE film_id = '$film_id'";
+    $query = "INSERT INTO moviefeature (title ,releaseDate,director,cast,img) VALUES ('$title','$releaseDate','$director','$cast','$file_to_saved')";
     $db->exec($query);
 
     // Display the Movie List page
--- FilmAdmin_CMS/FilmAdmin/update_movie.php
+++ FilmAdmin_CMS/FilmAdmin/update_movie.php
@@ -1,41 +1,38 @@
 <?php
-//connect to database
-use Project\Classes\DB\DB;
-$db = DB::getDB();
 
-//require_once('database.php');
+use Project\Classes\DB\DB;
+include '../../autoloader.php';
+$db = DB::getDB();
 // Get the value from the form
+//require_once('database.php');
+
 $film_id = $_POST['film_id'];
 $title = $_POST['title'];
 $releaseDate = $_POST['releaseDate'];
 $director = $_POST['director'];
 $cast = $_POST['cast'];
-//$img = $_POST['img'];
+//$img = $_POST['img1'];
 
-// Image
+// ====Get the Image
 $file_get = $_FILES['img1']['name'];
 $temp = $_FILES['img1']['tmp_name'];
 
-$file_to_saved = "../Assets/image/HomePage/".$file_get;
+$file_to_saved = "/Assets/image/HomePage/".$file_get;
 
 //Documents folder, should exist in your host in there you're going to save the file just uploaded
 
 move_uploaded_file($temp, $file_to_saved);
 
-//echo $file_to_saved;
-
-
-
- // Image part ends here
+//==================
 
 // Validate inputs
 if (empty($title) || empty($releaseDate) || empty($cast) || empty($director) || empty($file_get) ) {
-    $error = "Invalid Movie data. Check and fill all fields and then try again.";
+    $error = "Invalid Movie data. Check all fields and try again.";
     echo $error;
 } else {
-    // If valid, Add the movie to the database
+    // If valid, update the movie to the database
 
-    $query = "INSERT INTO moviefeature (title ,releaseDate,director,cast,img) VALUES ('$title','$releaseDate','$director','$cast','$file_to_saved')";
+    $query = "UPDATE moviefeature SET title ='$title',releaseDate='$releaseDate',director ='$director',cast='$cast',img='$file_to_saved' WHERE film_id = '$film_id'";
     $db->exec($query);
 
     // Display the Movie List page
