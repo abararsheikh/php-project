2abb0074ca7d18658dedd1646ec663a6d98cf4c9
Yi Zhao <zhao1max@gmail.com>
2016-04-22 16:58:51 -0400

    fix rating

--- FilmAdmin_CMS/View/IndexMovies_UserView.php
+++ FilmAdmin_CMS/View/IndexMovies_UserView.php
@@ -107,12 +107,12 @@ Release Date :
          </section>
      </main>
           <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="/Assets/js/bootstrap.min.js"></script>
+<!--    <script src="/Assets/js/bootstrap.min.js"></script>-->
     <script type="text/javascript" src="/Rating_System/rating.js"></script>
    <!-- <script type="text/javascript" src="/Rating_System/jquery.js"></script>-->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<!--        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
 
 
     </body>
