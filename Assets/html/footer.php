6791830ad6f53c314b6c0826c1318823d91179bb
Yi Zhao <zhao1max@gmail.com>
2016-04-22 14:06:05 -0400

    update header footer

--- Assets/html/footer.php
+++ Assets/html/footer.php
@@ -1,12 +1,10 @@
 <footer class="footer">
   <div class="well container">
     <div class="col-sm-4 text-center">
-      <?php
-      use Project\Classes\Router\Nav;
-
-      $footerNav = ['', '<p class="text-muted"><a href="%link%">%name%</a></p>', ''];
-      Nav::drawMenu('Homepage', $footerNav)
-      ?>
+      <p><a href="#">contact us</a></p>
+      <p><a href="#">customer service</a></p>
+      <p><a href="#">about us</a></p>
+      <p><a href="#">contact us</a></p>
     </div>
     <div class="col-sm-offset-4 text-center">
       <p><a href="#">contact us</a></p>
@@ -14,6 +12,7 @@
       <p><a href="#">about us</a></p>
       <p><a href="#">contact us</a></p>
     </div>
+
   </div>
 </footer>
 
